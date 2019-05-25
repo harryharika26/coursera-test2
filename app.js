3a89690730d61d38743921d4bd048d85febd570e
harika <harikabandaru26@gmail.com>
2019-05-25 13:39:43 +0530

    assignment web file

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
 (function(){
-  'use strict';
-  angular.module('NameCalculator',[])
-  .controller('NameCalculatorController',function($scope){
-    $scope.name = "";
-    $scope.totalValue = 0;
-
-    $scope.displayNumeric = function(){
-  var totalNameValue = calculateNumericForString($scope.name);
-  $scope.totalValue = totalNameValue;
-};
-
-function calculateNumericForString(string)
+  'use strict'
+  angular.module('myApp',[])
+  .controller('myCtrl',['$scope',function($scope){
+    $scope.name = '';
+    $scope.count ="";
+    var message = '';
+      $scope.mySplit = function(){
+        var comma = ',';
+        var fun = checkIfTooMuch($scope.name,comma);
+        $scope.count = fun;
+      }
+    function checkIfTooMuch(stringToSplit,seperator)
 {
-  var totalStringValue=0;
-  for(var i=0; i < string.length; i++)
-  {totalStringValue += string.charCodeAt(i);}
-  return totalStringValue;
+  var arr = stringToSplit.split(seperator);
+    var len = arr.length;
+
+      if (arr == '') {
+        message = "please enter the string";
+      }
+
+      else if(len >= 1 && len < 4)
+      {
+        message = "Enjoy!";
+      }
+      else if(len > 3)
+       {
+        message = "Too Much";
+       }
+       return message;
 };
-  });
+}]);
 })();
